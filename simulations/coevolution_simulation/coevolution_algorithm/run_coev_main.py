944bc5755c1de8c01963c38dadd63c4ac6222526
andres-leguizamon <andres.leguizamon251@gmail.com>
2026-03-30 21:57:36 -0500

    Cleaned up and generated dataset, fixed discrepancies across configurations and unified config files for references

--- simulations/coevolution_simulation/coevolution_algorithm/run_coev_main.py
+++
@@ -1,5 +0,0 @@
-if __name__ == "__main__":
-    test_yaml_path = (
-        Path(__file__).resolve().parents[2] / "inputs" / "environments" / "3_node_linear_graph_env_1.yaml"
-    )
-    df_results = run_coevolution(test_yaml_path)
